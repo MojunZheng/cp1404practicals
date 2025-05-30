2afcdc83fbccb13df0579a6ee7404916db130492
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-05-30 12:08:43 +0800

    Implement main menu loop with score validation and options

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -2,3 +2,27 @@ MENU = "(G)et a valid score\n (P)rint result\n (S)how stars\n (Q)uit"
 print(MENU)
 
 
+def main():
+    """main function"""
+    valid_score = " "
+    choice = input("Choose:").upper()
+    while choice != "Q":
+        if choice == "G":
+            valid_score = validate_score()
+        elif choice == "P":
+            if valid_score == "":
+                print("No score to determine")
+            else:
+                result = print_result(valid_score)
+                print(result)
+        elif choice == "S":
+            if valid_score == "":
+                print("No score to determine")
+            else:
+                print("*"*int(valid_score))
+        else:
+            print(MENU)
+            print("Error choice")
+        choice = input("Choose：").upper()
+    print("Bye")
+
