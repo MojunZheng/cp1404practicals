097868a158af7cb2c61608aa7574d09af884fc15
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-05-30 11:36:00 +0800

    Add determine_score

--- prac_02/score.py
+++ prac_02/score.py
@@ -12,4 +12,17 @@ def main():
     print(f"Your result is {result}")
 
 
+def determine_score(score):
+    """Determine the level of the inputted score and return a string result."""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score > 90:
+        result = "Excellent"
+    elif score > 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
 
+
+main()
