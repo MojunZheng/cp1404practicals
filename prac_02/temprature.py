42d9e3e9c891494860f35ccdb3150cdebbc16f1f
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-05-30 12:12:43 +0800

    Implement main loop for temperature conversion menu with input handling

--- prac_02/temprature.py
+++ prac_02/temprature.py
@@ -3,6 +3,20 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 
-
-
-
+def main():
+    """main function"""
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = calculate_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("fahrenheit: "))
+            celsius = calculate_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
