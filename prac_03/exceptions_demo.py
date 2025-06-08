8cc13cd82e42a6de1dc104b2bebe9fcae60d61b1
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-06-08 13:56:21 +0800

    Add docstring with answers explaining ValueError and ZeroDivisionError scenarios

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,9 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+    When enter something not a number
+2. When will a ZeroDivisionError occur?
+    When denominator is zero
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
