af4b42b392ab2670dd79bcb33337fad19aff74bc
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-06-08 14:42:55 +0800

    Read first two numbers from file and print their sum

--- prac_03/files.py
+++ prac_03/files.py
@@ -6,4 +6,8 @@ with open("name.txt", "r") as file:
     name = file.read()
     print(f"Hi {name}!")
 
+with open("numbers.txt", "r") as file:
+    number_1 = int(file.readline())
+    number_2 = int(file.readline())
+    print(number_1 + number_2)
 
