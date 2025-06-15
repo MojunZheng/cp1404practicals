2a6395c4e70c0081652557a9c52b83b68303a810
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-06-15 16:37:26 +0800

    Sort and format random numbers into quik pick groups for display

--- prac_04/quik_picks.py
+++ prac_04/quik_picks.py
@@ -7,3 +7,7 @@ for i in range(input_number):
         while random_number in random_numbers:
             random_number = random.randint(1, 45)
         random_numbers.append(random_number)
+for m in range(0, len(random_numbers), 5):
+    numbers = random_numbers[m:m+5]
+    numbers.sort()
+    print(" ".join(f"{number:>2}" for number in numbers))
