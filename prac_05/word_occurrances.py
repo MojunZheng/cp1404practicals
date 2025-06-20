a363f09cc73aab83c2105bbb64fd989d1ca0069f
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-06-20 09:50:14 +0800

    Count word occurrences and store in dictionary using exception handling

--- prac_05/word_occurrances.py
+++ prac_05/word_occurrances.py
@@ -2,3 +2,8 @@ dict_words = {}
 sentence = input("Text:")
 words = sentence.split(' ')
 words.sort()
+for word in words:
+    try:
+        dict_words[word] += 1
+    except KeyError:
+        dict_words[word] = 1
