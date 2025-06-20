5d1ceb57e2932221ad8431f6d00a5273c2ef8fb7
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-06-20 09:17:04 +0800

    Read data from CSV file and store each record in the info list

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -11,3 +11,11 @@ def main():
     dict_count_champions, win_countries = count_champions(wimbledon_winners)
     display_countries(win_countries, dict_count_champions)
 
+def get_file(filename):
+    """read the file"""
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        record = csv.reader(in_file)
+        next(record)
+        for i in record:
+            info.append(i)
+    return info
