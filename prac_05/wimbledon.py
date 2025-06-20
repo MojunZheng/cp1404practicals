2f8bf6e73a45447ac04fdeb9b3f082525da127b1
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-06-20 09:31:01 +0800

    Count each champion’s total wins and collect unique countries

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -19,3 +19,19 @@ def get_file(filename):
         for i in record:
             info.append(i)
     return info
+
+def count_champions(wimbledon_winners):
+    """count and return champions in dictionary and return countries in set"""
+    champions = []
+    countries = set()
+    for i in wimbledon_winners:
+        country = i[1]
+        athlete = i[2]
+        countries.add(country)
+        champions.append(athlete)
+    for name in champions:
+        try:
+            dict_champions[name] += 1
+        except KeyError:
+            dict_champions[name] = 1
+    return dict_champions, sorted(countries)
