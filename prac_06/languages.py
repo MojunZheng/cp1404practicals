46df9fb64112d235f09ce56d91768fbd3eba084a
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-06 19:57:44 +0800

    Add dynamic language filtering demo

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -8,3 +8,7 @@ print(python)
 languages = [ProgrammingLanguage("Python", "Dynamic", True, 1991),
              ProgrammingLanguage("Ruby", "Dynamic", True, 1995),
              ProgrammingLanguage("Visual Basic", "Static", False, 1991)]
+
+dynamic_languages = [language for language in languages if language.is_dynamic()]
+for language in dynamic_languages:
+    print(language)
