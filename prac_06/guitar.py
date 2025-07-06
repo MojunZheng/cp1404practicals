8c38e48c8ea88248963f66b05fa68a4302caf704
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-06 14:46:27 +0800

    Add is_vintage check (50+ years)

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -13,3 +13,7 @@ class Guitar:
     def get_age(self, now_year=0):
         """Calculates and returns the age of the guitar"""
         return now_year - self.year
+
+    def is_vintage(self):
+        """Determines if the guitar is older than 50 years."""
+        return self.get_age() >= 50
