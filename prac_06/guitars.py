b5c0282052ef9c94fb0595d4f4633703852fd7ba
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-06 15:13:46 +0800

    Implement guitar data input loop

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,3 +5,12 @@ guitars = []
 guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 print("My guitars!")
+
+name = input("Name:")
+while name != "":
+    year = int(input("Year:"))
+    cost = float(input("Cost: $"))
+    guitar = Guitar(name, year, cost)
+    print(f"{guitar} added.")
+    guitars.append(guitar)
+    name = input("Name:")
