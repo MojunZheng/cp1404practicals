a34af854c8af4a5165b23965d20e50725a2c1dd0
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-06 14:35:41 +0800

    Define Guitar class with basic attributes

---
+++ prac_06/guitar.py
@@ -0,0 +1,8 @@
+class Guitar:
+    """Create a Guitar class"""
+    def __init__(self, name="", year=0, cost=0.00):
+        """Initializes a Guitar object."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
