ae540c52d18f4d823668c00336571c39fc680966
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-06 14:56:33 +0800

    Implement age verification output

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -7,3 +7,6 @@ expected_vintage_1 = True
 guitar_2 = Guitar("Another Guitar", 2013, 1000.00)
 expected_age_2 = 11
 expected_vintage_2 = False
+
+print(f"{guitar_1.name} get_age() - Expected {expected_age_1}. Got {guitar_1.get_age()}")
+print(f"{guitar_2.name} get_age() - Expected {expected_age_2}. Got {guitar_2.get_age()}")
