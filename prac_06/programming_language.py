1a0148b6a07aa976b4baf30281c2ad8863d94488
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-06 20:00:16 +0800

    Implement __init__ with core attributes

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,3 +1,9 @@
 class ProgrammingLanguage:
     """Represent a ProgrammingLanguage object."""
 
+    def __init__(self, name, typing, reflection, year):
+        """Initialize a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
