a8121682898944004ab5200d387082f457b6dc9b
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-06 14:50:22 +0800

    Add test case for another modern guitar

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -3,3 +3,7 @@ from guitar import Guitar
 guitar_1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
 expected_age_1 = 102
 expected_vintage_1 = True
+
+guitar_2 = Guitar("Another Guitar", 2013, 1000.00)
+expected_age_2 = 11
+expected_vintage_2 = False
