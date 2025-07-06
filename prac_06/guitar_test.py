caeebec7353beaba07b171767675ada47deeb133
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-06 15:01:11 +0800

    Add vintage status verification output

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -10,3 +10,6 @@ expected_vintage_2 = False
 
 print(f"{guitar_1.name} get_age() - Expected {expected_age_1}. Got {guitar_1.get_age()}")
 print(f"{guitar_2.name} get_age() - Expected {expected_age_2}. Got {guitar_2.get_age()}")
+
+print(f"{guitar_1.name} is_vintage() - Expected {expected_vintage_1}. Got {guitar_1.is_vintage()}")
+print(f"{guitar_2.name} is_vintage() - Expected {expected_vintage_2}. Got {guitar_2.is_vintage()}")
