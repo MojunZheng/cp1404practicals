c5f063181e23f376a481dee4ddd2cb3904160116
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-11 17:26:43 +0800

    Add read_file function to load guitar data from CSV and create Guitar objects

--- prac_07/myGuitar.py
+++ prac_07/myGuitar.py
@@ -1,1 +1,11 @@
 from guitar import Guitar
+
+def read_file():
+    """Reads guitar data from guitars CSV file"""
+    guitars = []
+    with open("guitars.csv", "r") as guitars_information:
+        for line in guitars_information:
+            parts = line.strip().split(",")
+            guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+            guitars.append(guitar)
+    return guitars
