29e2d0e4fb98770a67a9cd333cae768a27b62918
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-11 10:07:45 +0800

    Import csv, namedtuple, and custom ProgrammingLanguage class

---
+++ prac_07/language_file_reader.py
@@ -0,0 +1,10 @@
+"""
+CP1404/CP5632 Practical
+File and class example - opens/reads a file, stores in objects of custom class
+(contains multiple versions for demonstration: using csv and namedtuple)
+"""
+
+import csv
+from collections import namedtuple
+from programming_language import ProgrammingLanguage
+
