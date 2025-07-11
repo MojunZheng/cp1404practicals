5197bd12b1887aaefa53ce88016840ccab2dd814
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-11 12:31:38 +0800

    Define COMPARE_YEAR constant for vintage guitar age threshold Add Guitar class with constructor to initialize name, year, and cost

---
+++ prac_07/guitar.py
@@ -0,0 +1,10 @@
+COMPARE_YEAR = 50
+
+
+class Guitar:
+    """Create a Guitar class"""
+    def __init__(self, name="", year=0, cost=0.00):
+        """Initializes a Guitar object."""
+        self.name = name
+        self.year = year
+        self.cost = cost
