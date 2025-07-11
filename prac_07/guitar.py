7f6cb6dbab131cdcc3201ba49f6f8cc39108be02
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-11 16:33:17 +0800

    Implement __str__ method to return formatted string representation of a Guitar Add get_age method to calculate the guitar's age given the current year

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -8,3 +8,11 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Returns a formatted string of the guitar"""
+        return f"{self.name} ({self.year}): ${self.cost}"
+
+    def get_age(self, now_year=0):
+        """Calculates and returns the age of the guitar"""
+        return now_year - self.year
