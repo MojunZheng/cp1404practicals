7485c74a26f75079b1b76dc89ef04f196a8891d9
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-11 11:16:28 +0800

    Define ProgrammingLanguage class with initializer to store language attributes

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -6,3 +6,13 @@ from collections import namedtuple
 import csv
 
 
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
+
+    def __init__(self, name, typing, reflection, year, pointer):
+        """Construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.pointer = pointer
