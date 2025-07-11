c46bd5c833d5b7b1aadc6b0692ecce70ee0ad69c
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-11 17:51:21 +0800

    Create Project class with attributes: name, start_date, priority, cost_estimate, and percentage_complete

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,2 +1,11 @@
 MAXIMUM_PERCENTAGE = 100
 
+class Project:
+    """Create a project class"""
+
+    def __init__(self, name, start_date, priority=0, cost_estimate=0.0, percentage=0):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.percentage = percentage
