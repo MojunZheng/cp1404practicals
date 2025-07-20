725f2c45adaaa6d888fb45fad5c23f3ad26c727b
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-20 08:17:38 +0800

    Load UI layout from box_layout.kv and set window title in build()

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,3 +4,8 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     """BoxLayoutDemo is a demo for boxlayout"""
+    def build(self):
+        """build a kivy app from the kivy file"""
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
