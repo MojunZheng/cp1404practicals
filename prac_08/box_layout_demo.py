d1e732f4237eb84c239333a75246175be32d1be2
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-20 08:41:26 +0800

    Define output_label method to show greeting with user input

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -9,3 +9,7 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def output_label(self):
+        """create greeting string to label"""
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
