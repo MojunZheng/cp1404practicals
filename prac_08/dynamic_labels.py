9a8a08474b9db8c1f114c56bcf8f424b56a40f19
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-20 12:40:53 +0800

    Import Kivy modules and define main App class with docstring

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    """Main program - Kivy app to demo dynamic widget creation."""
