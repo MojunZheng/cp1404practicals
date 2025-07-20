96c5dc852b2a14729b24332bbbdc1817686cc99a
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-20 09:43:54 +0800

    Add initial comments, import Kivy modules, and define MILES_TO_KM constant

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,12 @@
+"""
+CP1404 Week 11 Workshop - GUI program to convert miles to kilometres
+Lindsay Ward, IT@JCU
+06/10/2015
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+
+MILES_TO_KM = 1.60934
