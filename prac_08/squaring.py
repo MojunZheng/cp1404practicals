6e49f4f3b294a1aa81e5e6bea16cf9bcb484dd70
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-20 18:01:07 +0800

    Define SquareNumberApp class with GUI build method, load KV layout, and set window title and size

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -11,3 +11,11 @@ from kivy.core.window import Window
 
 __author__ = 'Lindsay Ward'
 
+class SquareNumberApp(App):
+    """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (200, 100)
+        self.title = "Square Number"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
