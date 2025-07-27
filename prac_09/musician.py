967276b45b62e2a9cea4a63dfffe0c6f467bbc73
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 14:30:37 +0800

    Add __str__ method for string representation of Musician

--- prac_09/musician.py
+++ prac_09/musician.py
@@ -6,3 +6,6 @@ def __init__(self, name=""):
     """Construct a Musician with a name and empty instrument collection."""
     self.name = name
     self.instruments = []
+def __str__(self):
+    """Return a string representation of a Musician."""
+    return f"{self.name} ({self.instruments})"
