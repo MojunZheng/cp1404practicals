da29fe428c4ce04d6c30957d3a56db460fcbb670
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 15:02:14 +0800

    Add module docstring and import trees module for testing various tree subclasses

---
+++ prac_09/trees_tester.py
@@ -0,0 +1,7 @@
+"""
+CP1404/CP5632 Practical
+Testing/example client code for trees classes
+When you complete all the subclasses, you'll see that they behave differently.
+
+"""
+import prac_09.trees as trees
