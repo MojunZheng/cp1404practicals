90e41fe64c5701c1f8b7aaf9feebbf39d20ad3ff
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 14:31:02 +0800

    Add __repr__ method to return internal state of Musician as string

--- prac_09/musician.py
+++ prac_09/musician.py
@@ -9,3 +9,6 @@ def __init__(self, name=""):
 def __str__(self):
     """Return a string representation of a Musician."""
     return f"{self.name} ({self.instruments})"
+def __repr__(self):
+    """Return a string representation of a Musician, showing the variables."""
+    return str(vars(self))
