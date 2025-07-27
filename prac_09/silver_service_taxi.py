844ac659f341de45707ca83f7acb787acbd546d2
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 14:41:27 +0800

    Override get_fare method to include flagfall in fare calculation

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -11,3 +11,6 @@ class SilverServiceTaxi(Taxi):
     def __str__(self):
         """Return a string representation of a SilverServiceTaxi."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+    def get_fare(self):
+        """Get the current fare."""
+        return self.flagfall + super().get_fare()
