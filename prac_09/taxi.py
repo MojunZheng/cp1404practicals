8688910e2a5d615f2f6e400e0dd5558a461f856f
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 14:49:50 +0800

    Override __str__ method to include current fare distance and price per km in string representation

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -10,3 +10,6 @@ class Taxi(Car):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.current_fare_distance = 0
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
