541de0c352d791bab8ad9289fb192aecd9883625
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 14:40:34 +0800

    Initialize SilverServiceTaxi with fanciness and adjust price per km

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -4,3 +4,7 @@ class SilverServiceTaxi(Taxi):
     """Created a SilverServiceTaxi class"""
     flagfall = 4.5
 
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
