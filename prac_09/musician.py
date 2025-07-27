a38aa3043e1e2c0e70586f2fae457800e58faf38
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 14:34:32 +0800

    finish

--- prac_09/musician.py
+++ prac_09/musician.py
@@ -3,7 +3,6 @@
 
 class Musician:
     """Musician class"""
-    
     def __init__(self, name=""):
         """Construct a Musician with a name and empty instrument collection."""
         self.name = name
