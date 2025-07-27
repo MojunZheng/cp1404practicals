3e756c0c2910ec27c12cbfddef842c586eea248d
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 14:29:51 +0800

    Implement __init__ method to initialize name and instruments list

--- prac_09/musician.py
+++ prac_09/musician.py
@@ -2,3 +2,7 @@
 
 class Musician:
     """Musician class"""
+def __init__(self, name=""):
+    """Construct a Musician with a name and empty instrument collection."""
+    self.name = name
+    self.instruments = []
