758686a016456bc42467245a551b9ad7d2635384
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 14:24:42 +0800

    Add __str__ method for string representation of Guitar

--- prac_09/guitar.py
+++ prac_09/guitar.py
@@ -12,3 +12,6 @@ def __init__(self, name="", year=0, cost=0):
     self.name = name
     self.year = year
     self.cost = cost
+def __str__(self):
+    """Return a string representation of a Guitar."""
+    return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
