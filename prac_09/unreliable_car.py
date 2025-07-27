d02bd93d062b5af24330238e00d73dce88e21091
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-07-27 15:04:37 +0800

    Override drive method to drive only if random chance is below reliability threshold; otherwise do not drive

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -8,3 +8,10 @@ class UnreliableCar(Car):
         super().__init__(name, fuel)
         self.reliability = float(reliability)
         self.drive_distance = 0
+    def drive(self, distance):
+        """Drive the car when random number is less than reliability"""
+        if random.randint(1, 100) < self.reliability:
+            drive_distance = super().drive(distance)
+        else:
+            drive_distance = 0
+        return drive_distance
