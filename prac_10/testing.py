273c5b6c5e3ac4f976ca27fd4b50ac10fdbd361c
MojunZheng <mojun.zheng@my.jcu.edu.au>
2025-08-03 11:03:28 +0800

    Define repeat_string function to repeat input string s n times separated by spaces

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -6,3 +6,6 @@ Testing code using assert and doctest
 import doctest
 from prac_06.car import Car
 
+def repeat_string(s, n):
+    """Repeat string s, n times, with spaces in between."""
+    return " ".join([s] * n)
